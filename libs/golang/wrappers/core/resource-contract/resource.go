70f56c477f21f8b36bfd0705178607c41acfc2d9
Fabio Caffarello <fabio.caffarello@gmail.com>
2024-06-06 21:27:05 +0000

    feat: create resource wrapper interface

--- libs/golang/wrappers/core/resource-contract/resource.go
+++ libs/golang/wrappers/core/resource-contract/resource.go
@@ -2,9 +2,9 @@ package wrappersresourcecontract
 
 // Resource defines the interface that all resources should implement
 type Resource interface {
-    // Init initializes the resource and returns an error if any occurs.
-    Init() error
-    
-    // GetClient returns the underlying client of the resource.
-    GetClient() interface{}
+	// Init initializes the resource and returns an error if any occurs.
+	Init() error
+
+	// GetClient returns the underlying client of the resource.
+	GetClient() interface{}
 }
