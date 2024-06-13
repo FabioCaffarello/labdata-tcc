66024adb14a38f04ea203fca2cf05e9576d984d8
Fabio Caffarello <fabio.caffarello@gmail.com>
2024-06-13 14:03:01 +0000

    feat: create repository interface

--- libs/golang/ddd/domain/entities/config-vault/entity/repository_implementation.go
+++ libs/golang/ddd/domain/entities/config-vault/entity/repository_implementation.go
@@ -1,4 +1,15 @@
 package entity
 
 type ConfigRepositoryInterface interface {
+	Create(config *Config) error
+	FindByID(id string) (*Config, error)
+	FindAll() ([]*Config, error)
+	Update(config *Config) error
+	Delete(id string) error
+	FindAllByService(service string) ([]*Config, error)
+	FindAllBySource(source string) ([]*Config, error)
+	FindAllByServiceAndSource(service, source string) ([]*Config, error)
+	FindAllByServiceAndSourceAndProvider(service, source, provider string) ([]*Config, error)
+	FindAllByServiceAndProviderAndActive(service, provider string, active bool) ([]*Config, error)
+	FindAllByDependsOn(dependsOn map[string]interface{}) ([]*Config, error)
 }
