0e9c1276d351362717c6a4f480bd0d1bb68f86f2
Fabio Caffarello <fabio.caffarello@gmail.com>
2024-07-08 02:36:41 +0000

    feat(golang): update events-router dto with input-id

--- libs/golang/ddd/dtos/events-router/output/dto.go
+++ libs/golang/ddd/dtos/events-router/output/dto.go
@@ -11,11 +11,12 @@ type ErrMsgDTO struct {
 // It includes the necessary details required for processing an order,
 // such as the processing ID, service, source, provider, stage, and data.
 type ProcessOrderDTO struct {
-	ID           string                 `json:"id"`            // ID represents the unique identifier of the order.
+	ID           string                 `json:"_id"`           // ID represents the unique identifier of the order.
 	ProcessingID string                 `json:"processing_id"` // ProcessingID represents the unique identifier of the order processing.
 	Service      string                 `json:"service"`       // Service represents the name of the service for which the order is processed.
 	Source       string                 `json:"source"`        // Source indicates the origin or source of the order.
 	Provider     string                 `json:"provider"`      // Provider specifies the provider of the order.
 	Stage        string                 `json:"stage"`         // Stage represents the current stage of the order processing.
+	InputID      string                 `json:"input_id"`      // InputID represents the unique identifier of the input data.
 	Data         map[string]interface{} `json:"data"`          // Data represents the order data.
 }
