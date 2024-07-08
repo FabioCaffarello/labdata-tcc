9249265d9056978773d6f727fc2d94ca27da902b
Fabio Caffarello <fabio.caffarello@gmail.com>
2024-07-08 02:37:00 +0000

    feat(golang): update events-router dto status

--- libs/golang/ddd/dtos/input-broker/input/dto.go
+++ libs/golang/ddd/dtos/input-broker/input/dto.go
@@ -1,18 +1,9 @@
 package inputdto
 
-import (
-	shareddto "libs/golang/ddd/dtos/input-broker/shared"
-)
-
 // InputDTO represents the input data transfer object.
 type InputDTO struct {
 	Provider string                 `json:"provider"` // Provider represents the provider of the input data.
 	Service  string                 `json:"service"`  // Service represents the service of the input data.
 	Source   string                 `json:"source"`   // Source represents the source of the input data.
 	Data     map[string]interface{} `json:"data"`     // Data represents the input data.
-}
-
-// StatusDTO represents the status data transfer object.
-type StatusDTO struct {
-	shareddto.StatusDTO `json:"status"` // Status represents the status of the input data.
 }
