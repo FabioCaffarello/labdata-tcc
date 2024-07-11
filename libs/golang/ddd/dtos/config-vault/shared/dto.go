47ff270838b57fe52329fbb5194e671c82f21942
Fabio Caffarello <fabio.caffarello@gmail.com>
2024-07-11 21:53:50 +0000

    feat: update Config dto with job params

--- libs/golang/ddd/dtos/config-vault/shared/dto.go
+++ libs/golang/ddd/dtos/config-vault/shared/dto.go
@@ -6,3 +6,9 @@ type JobDependenciesDTO struct {
 	Service string `json:"service"` // Service represents the name of the dependent service.
 	Source  string `json:"source"`  // Source indicates the origin or source of the dependency.
 }
+
+// JobParametersDTO represents the data transfer object for job parameters.
+// It includes the parser module that is used to parse the configuration.
+type JobParametersDTO struct {
+	ParserModule string `json:"parser_module"` // ParserModule specifies the module used for parsing the configuration.
+}
